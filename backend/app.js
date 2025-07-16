d0956ebaa8fd0d8b618c163971a7620eaa9906f1
Anurag1111111 <anujangir1707@gmail.com>
2025-07-16 14:06:28 +0530

    app.js fix

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import rateLimit from 'express-rate-limit';
 
 // Route file
 import orderRoutes from "./routes/orderRoutes.js";
@@ -15,14 +14,6 @@ export const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-
-// Rate Limiter
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 mins
-  max: 100, // Limit each IP to 100 requests
-  message: 'Too many requests, try again later.'
-});
-app.use(limiter);
 
 // CORS
 app.use(
